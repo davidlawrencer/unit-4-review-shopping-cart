aec1ce5901e75989443305590470ba7a413cf9e4
davidlawrencer <davidlrifkin+github@gmail.com>
2019-10-23 14:31:50 -0400

    Set root view controller and comment in project requirements

---
+++ unit-4-review/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  unit-4-review
+//
+//  Created by David Rifkin on 10/23/19.
+//  Copyright © 2019 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    // MARK: Views
+    //collection view
+    //some kind of navigation
+    //navigation bar title (to show name)
+    //nav bar button
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+}
+
+//custom delegation
+//user defaults
+//gesture recognizers
+//animations
